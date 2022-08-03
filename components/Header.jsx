31a7411e1d00839599bc21b2ae10a980f1c14ab1
natnael-OQP <makiya112545@gmail.com>
2022-08-03 20:06:19 +0300

    style header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,18 +1,27 @@
+import Image from 'next/image';
 import React from 'react';
 
 function Header() {
     return (
-        <div className="h-14 w-full shadow-sm bg-white flex flex-row justify-between items-center px-10">
+        <div className="max-w-7xl z-50 h-20 w-full mx-auto    flex flex-row justify-between items-center px-10 absolute top-0 left-0">
             {/*logo*/}
-            <div>
-                <h1 className="text-xl text-sky-400 font-Pop">Logo</h1>
+            <div className=" relative h-full w-[200px] z-55 ml-10">
+                <Image
+                    layout="fill"
+                    objectFit="contain"
+                    src="/images/logo.png"
+                    alt="logo"
+                />
             </div>
             {/* navbar*/}
-            <div className=" fx space-x-3  ">
-                <h1 className="text-lg text-slate-600 font-Pop  ">Home</h1>
-                <h1 className="text-lg text-slate-600 font-Pop">About</h1>
-                <h1 className="text-lg text-slate-600 font-Pop">service</h1>
-                <h1 className="text-lg text-slate-600 font-Pop">Contact</h1>
+            <div className=" relative h-6 w-[300px] z-55  ">
+                <Image
+                    layout="fill"
+                    objectFit="contain"
+                    src="/images/socialmediaicon.png"
+                    alt="logo"
+                    className="z-55"
+                />
             </div>
         </div>
     );
