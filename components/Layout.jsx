e38ee5e779b7ed54ed8122a0b173cafab5abeb5a
natnael-OQP <makiya112545@gmail.com>
2022-08-04 15:12:58 +0300

    style header page

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
+import { useRouter } from 'next/router';
 
 function Layout({ children }) {
+    const route = useRouter();
+    const { pathname } = route;
+
     return (
-        <div className="max-w-7xl mx-auto ">
+        <div
+            className={`mx-auto max-w-8xl overflow-hidden ${
+                pathname == '/black' && 'bg-black'
+            } `}
+        >
             <Header />
             <main>{children}</main>
             {/*
